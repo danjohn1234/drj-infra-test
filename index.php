dc9c292a8d0f8852221ecaaca53f1dbaa53726ed
Johnson <mrf8180@r5048536.mayo.edu>
2016-07-13 14:14:29 -0500

    Test Db Conn. Fix. Again.

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 
 <?php
 try {
-	$conn = new PDO ( \"sqlsrv:server = tcp:drjtest-helix-sql1.database.windows.net,1433; Database = drjtest-helix-nc-sql\", \"danjohn1234\", \"{your_password_here}\");
+	$conn = new PDO ("sqlsrv:server = tcp:drjtest-helix-sql1.database.windows.net,1433; Database = drjtest-helix-nc-sql", "danjohn1234", "#EDC4rfv");
 	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
+
 catch ( PDOException $e ) {
-	 print( \"Error connecting to SQL Server.\" );
+	 print("Error connecting to SQL Server.");
 	 die(print_r($e));
 }
 
-$connectionInfo = array(\"UID\" => \"danjohn1234@drjtest-helix-sql1\", \"pwd\" => \"{your_password_here}\", \"Database\" => \"drjtest-helix-nc-sql\", \"LoginTimeout\" => 30, \"Encrypt\" => 1, \"TrustServerCertificate\" => 0);
-$serverName = \"tcp:drjtest-helix-sql1.database.windows.net,1433\";
+$connectionInfo = array("UID" => "danjohn1234@drjtest-helix-sql1", "pwd" => "#EDC4rfv", "Database" => "drjtest-helix-nc-sql", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:drjtest-helix-sql1.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 
